873d94b5bf41c96f273fca4d77cf015d4dfdae27
Congzhiwei1994 <z1039329737@qq.com>
2021-08-26 21:46:04 +0800

    11
    11

---
+++ 2020.3.15f2_main/Assets/3rd/VertexPainter_2021.1/Script/MeshVertexColor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Jefford.TA {
+	[Serializable]
+	public class MeshVertexColor : ScriptableObject {
+
+		[SerializeField]
+		public Color[] _colors;
+	}
+
+}
