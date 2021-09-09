27ee9b343cf6a45e0a036644733eb343ac2eb6ef
congzhiwei1994 <75684067+congzhiwei1994@users.noreply.github.com>
2021-09-09 18:44:20 +0800

    平面反射脚本

---
+++ 2020.3.15f2_main/Assets/Rendering/Scripts/KawaseBlur.shader
@@ -0,0 +1,79 @@
+﻿Shader "Hidden/KawaseBlur(模糊)"
+{
+	Properties
+	{
+		_MainTex("", 2D) = "white" {}
+	}
+	SubShader
+	{	
+		Tags 
+		{ 
+			"RenderPipeline" = "UniversalPipeline" 
+		}
+		
+		Pass
+		{
+			Cull Off ZWrite Off ZTest Always
+
+			HLSLPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+
+			#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+			#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/CommonMaterial.hlsl"
+			#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/SurfaceInput.hlsl"
+			#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
+
+
+
+			float4 _MainTex_TexelSize;
+			uniform half _Offset;
+
+			TEXTURE2D(_MainTex);          SAMPLER(sampler_MainTex);
+
+			struct Attributes
+			{
+
+				float4 positionOS        : POSITION;
+				float2 uv               : TEXCOORD0;
+
+			};
+
+			struct Varyings
+			{
+				float4 positionCS         : SV_POSITION;
+				float2 uv                 : TEXCOORD0;
+			};
+
+
+			Varyings vert(Attributes v)
+			{
+				Varyings o = (Varyings)0;
+				o.uv = v.uv;
+				o.positionCS = TransformObjectToHClip(v.positionOS.xyz);
+				return o;
+			}
+			
+			
+			half4 frag(Varyings i): SV_Target
+			{
+
+				half2 uv1 = i.uv + float2(_Offset + 0.5, _Offset + 0.5) * _MainTex_TexelSize.xy;
+				half2 uv2 = i.uv + float2(-_Offset - 0.5, _Offset + 0.5) * _MainTex_TexelSize.xy;
+				half2 uv3 = i.uv + float2(-_Offset - 0.5, -_Offset - 0.5) * _MainTex_TexelSize.xy;
+				half2 uv4 = i.uv + float2(_Offset + 0.5, -_Offset - 0.5) * _MainTex_TexelSize.xy;
+
+				half4 o = 0;
+				o += SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv1);
+				o += SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv2);
+				o += SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv3);
+				o += SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv4);
+				
+				return o * 0.25;
+			}
+			ENDHLSL
+		}
+	}
+}
+
+
--- 2020.3.15f2_main/Assets/Rendering/Scripts/PlanarReflectionManager.cs
+++ 2020.3.15f2_main/Assets/Rendering/Scripts/PlanarReflectionManager.cs
@@ -7,156 +7,178 @@ using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
 [ExecuteInEditMode]
-public class PlanarReflectionManager : MonoBehaviour {
+public class PlanarReflectionManager : MonoBehaviour
+{
 
-    [Range (0.01f, 1f)] public float renderScale = 1f;
+    [Range(0.01f, 1f)] public float renderScale = 1f;
     public LayerMask reflectionLayer = -1;
     public bool reflectSkybox;
     public GameObject reflectionTarget;
-    [Range (-2f, 3f)] public float reflectionPlaneOffset;
+    [Range(-2f, 3f)] public float reflectionPlaneOffset;
     private static Camera _reflectionCamera;
     UnityEngine.Rendering.Universal.UniversalAdditionalCameraData cameraData;
     private static RenderTexture _reflectionTexture;
     RenderTextureDescriptor previousDescriptor;
-    private readonly int _planarReflectionTextureId = Shader.PropertyToID ("_PlanarReflectionTexture");
+    private readonly int _planarReflectionTextureId = Shader.PropertyToID("_PlanarReflectionTexture");
     public static event Action<ScriptableRenderContext, Camera> BeginPlanarReflections;
     public bool hideReflectionCamera;
 
-    void OnEnable () {
+    void OnEnable()
+    {
         RenderPipelineManager.beginCameraRendering += DoPlanarReflections;
         reflectionLayer = ~(1 << 4);
     }
 
-    void OnDisable () {
-        CleanUp ();
+    void OnDisable()
+    {
+        CleanUp();
         RenderPipelineManager.beginCameraRendering -= DoPlanarReflections;
     }
 
-    void OnDestroy () {
-        CleanUp ();
+    void OnDestroy()
+    {
+        CleanUp();
         RenderPipelineManager.beginCameraRendering -= DoPlanarReflections;
     }
 
-    void CleanUp () {
-        if (_reflectionCamera) {
+    void CleanUp()
+    {
+        if (_reflectionCamera)
+        {
             _reflectionCamera.targetTexture = null;
-            SafeDestroyObject (_reflectionCamera.gameObject);
+            SafeDestroyObject(_reflectionCamera.gameObject);
         }
 
-        if (_reflectionTexture) RenderTexture.ReleaseTemporary (_reflectionTexture);
+        if (_reflectionTexture) RenderTexture.ReleaseTemporary(_reflectionTexture);
 
     }
 
-    void SafeDestroyObject (UnityEngine.Object obj) {
-        if (Application.isEditor) DestroyImmediate (obj);
-        else Destroy (obj);
+    void SafeDestroyObject(UnityEngine.Object obj)
+    {
+        if (Application.isEditor) DestroyImmediate(obj);
+        else Destroy(obj);
     }
 
-    private void UpdateReflectionCamera (Camera realCamera) {
-        if (_reflectionCamera == null) _reflectionCamera = InitializeReflectionCamera ();
+    private void UpdateReflectionCamera(Camera realCamera)
+    {
+        if (_reflectionCamera == null) _reflectionCamera = InitializeReflectionCamera();
 
         Vector3 pos = Vector3.zero;
         Vector3 normal = Vector3.up;
 
-        if (reflectionTarget != null) {
+        if (reflectionTarget != null)
+        {
             pos = reflectionTarget.transform.position + Vector3.up * reflectionPlaneOffset;
             normal = reflectionTarget.transform.up;
         }
 
-        UpdateCamera (realCamera, _reflectionCamera);
+        UpdateCamera(realCamera, _reflectionCamera);
         _reflectionCamera.gameObject.hideFlags = (hideReflectionCamera) ? HideFlags.HideAndDontSave : HideFlags.DontSave;
 #if UNITY_EDITOR
-        EditorApplication.DirtyHierarchyWindowSorting ();
+        EditorApplication.DirtyHierarchyWindowSorting();
 #endif
 
-        var d = -Vector3.Dot (normal, pos);
-        var reflectionPlane = new Vector4 (normal.x, normal.y, normal.z, d);
+        var d = -Vector3.Dot(normal, pos);
+        var reflectionPlane = new Vector4(normal.x, normal.y, normal.z, d);
 
         var reflection = Matrix4x4.identity;
-        reflection *= Matrix4x4.Scale (new Vector3 (1, -1, 1));
+        reflection *= Matrix4x4.Scale(new Vector3(1, -1, 1));
 
-        PlanarReflectionManager.CalculateReflectionMatrix (ref reflection, reflectionPlane);
-        var oldPosition = realCamera.transform.position - new Vector3 (0, pos.y * 2, 0);
-        var newPosition = PlanarReflectionManager.ReflectPosition (oldPosition);
-        _reflectionCamera.transform.forward = Vector3.Scale (realCamera.transform.forward, new Vector3 (1, -1, 1));
+        PlanarReflectionManager.CalculateReflectionMatrix(ref reflection, reflectionPlane);
+        var oldPosition = realCamera.transform.position - new Vector3(0, pos.y * 2, 0);
+        var newPosition = PlanarReflectionManager.ReflectPosition(oldPosition);
+        _reflectionCamera.transform.forward = Vector3.Scale(realCamera.transform.forward, new Vector3(1, -1, 1));
         _reflectionCamera.worldToCameraMatrix = realCamera.worldToCameraMatrix * reflection;
 
-        var clipPlane = CameraSpacePlane (_reflectionCamera, pos - Vector3.up * 0.1f, normal, 1.0f);
-        var projection = realCamera.CalculateObliqueMatrix (clipPlane);
+        var clipPlane = CameraSpacePlane(_reflectionCamera, pos - Vector3.up * 0.1f, normal, 1.0f);
+        var projection = realCamera.CalculateObliqueMatrix(clipPlane);
         _reflectionCamera.projectionMatrix = projection;
         _reflectionCamera.cullingMask = reflectionLayer;
         _reflectionCamera.transform.position = newPosition;
     }
 
-    private void UpdateCamera (Camera src, Camera dest) {
+    private void UpdateCamera(Camera src, Camera dest)
+    {
         if (dest == null) return;
 
-        dest.CopyFrom (src);
+        dest.CopyFrom(src);
         dest.useOcclusionCulling = false;
 
-        if (dest.gameObject.TryGetComponent (out UnityEngine.Rendering.Universal.UniversalAdditionalCameraData camData)) {
+        if (dest.gameObject.TryGetComponent(out UnityEngine.Rendering.Universal.UniversalAdditionalCameraData camData))
+        {
             camData.renderShadows = false;
             if (reflectSkybox) dest.clearFlags = CameraClearFlags.Skybox;
-            else {
+            else
+            {
                 dest.clearFlags = CameraClearFlags.SolidColor;
                 dest.backgroundColor = Color.black;
             }
         }
     }
 
-    private Camera InitializeReflectionCamera () {
-        var go = new GameObject ("", typeof (Camera));
-        go.name = "Reflection Camera [" + go.GetInstanceID () + "]";
-        var camData = go.AddComponent (typeof (UnityEngine.Rendering.Universal.UniversalAdditionalCameraData)) as UnityEngine.Rendering.Universal.UniversalAdditionalCameraData;
+    private Camera InitializeReflectionCamera()
+    {
+        var go = new GameObject("", typeof(Camera));
+        go.name = "Reflection Camera [" + go.GetInstanceID() + "]";
+        var camData = go.AddComponent(typeof(UnityEngine.Rendering.Universal.UniversalAdditionalCameraData)) as UnityEngine.Rendering.Universal.UniversalAdditionalCameraData;
 
         camData.requiresColorOption = CameraOverrideOption.Off;
         camData.requiresDepthOption = CameraOverrideOption.Off;
-        camData.SetRenderer (0);
+        camData.SetRenderer(0);
 
         var t = transform;
-        var reflectionCamera = go.GetComponent<Camera> ();
-        reflectionCamera.transform.SetPositionAndRotation (t.position, t.rotation);
+        var reflectionCamera = go.GetComponent<Camera>();
+        reflectionCamera.transform.SetPositionAndRotation(t.position, t.rotation);
         reflectionCamera.depth = -10;
         reflectionCamera.enabled = false;
 
         return reflectionCamera;
     }
 
-    private Vector4 CameraSpacePlane (Camera cam, Vector3 pos, Vector3 normal, float sideSign) {
+    private Vector4 CameraSpacePlane(Camera cam, Vector3 pos, Vector3 normal, float sideSign)
+    {
         var m = cam.worldToCameraMatrix;
-        var cameraPosition = m.MultiplyPoint (pos);
-        var cameraNormal = m.MultiplyVector (normal).normalized * sideSign;
-        return new Vector4 (cameraNormal.x, cameraNormal.y, cameraNormal.z, -Vector3.Dot (cameraPosition, cameraNormal));
+        var cameraPosition = m.MultiplyPoint(pos);
+        var cameraNormal = m.MultiplyVector(normal).normalized * sideSign;
+        return new Vector4(cameraNormal.x, cameraNormal.y, cameraNormal.z, -Vector3.Dot(cameraPosition, cameraNormal));
     }
 
-    RenderTextureDescriptor GetDescriptor (Camera camera, float pipelineRenderScale) {
-        var width = (int) Mathf.Max (camera.pixelWidth * pipelineRenderScale * renderScale);
-        var height = (int) Mathf.Max (camera.pixelHeight * pipelineRenderScale * renderScale);
+    RenderTextureDescriptor GetDescriptor(Camera camera, float pipelineRenderScale)
+    {
+        var width = (int)Mathf.Max(camera.pixelWidth * pipelineRenderScale * renderScale);
+        var height = (int)Mathf.Max(camera.pixelHeight * pipelineRenderScale * renderScale);
         var hdr = camera.allowHDR;
         var renderTextureFormat = hdr ? RenderTextureFormat.DefaultHDR : RenderTextureFormat.Default;
 
-        return new RenderTextureDescriptor (width, height, renderTextureFormat, 16) {
+        return new RenderTextureDescriptor(width, height, renderTextureFormat, 16)
+        {
             autoGenerateMips = true,
-                useMipMap = true
+            useMipMap = true
         };
     }
 
-    private void CreateReflectionTexture (Camera camera) {
-        var descriptor = GetDescriptor (camera, UniversalRenderPipeline.asset.renderScale);
+    private void CreateReflectionTexture(Camera camera)
+    {
+        var descriptor = GetDescriptor(camera, UniversalRenderPipeline.asset.renderScale);
 
-        if (_reflectionTexture == null) {
-            _reflectionTexture = RenderTexture.GetTemporary (descriptor);
+        if (_reflectionTexture == null)
+        {
+            _reflectionTexture = RenderTexture.GetTemporary(descriptor);
             previousDescriptor = descriptor;
-        } else if (!descriptor.Equals (previousDescriptor)) {
-            if (_reflectionTexture) RenderTexture.ReleaseTemporary (_reflectionTexture);
+        }
 
-            _reflectionTexture = RenderTexture.GetTemporary (descriptor);
+        else if (!descriptor.Equals(previousDescriptor))
+        {
+            if (_reflectionTexture) RenderTexture.ReleaseTemporary(_reflectionTexture);
+
+            _reflectionTexture = RenderTexture.GetTemporary(descriptor);
             previousDescriptor = descriptor;
         }
         _reflectionCamera.targetTexture = _reflectionTexture;
     }
 
-    private void DoPlanarReflections (ScriptableRenderContext context, Camera camera) {
+    private void DoPlanarReflections(ScriptableRenderContext context, Camera camera)
+    {
         /* If reference position and proxy position is exactly the same, we end up in some degeneracies triggered
            by engine code when computing culling parameters. This is an extremely rare case, but can happen
            in editor when focusing on the planar probe. So if that happens, we offset them 0.1 mm apart.
@@ -168,36 +190,46 @@ public class PlanarReflectionManager : MonoBehaviour {
         if (camera.cameraType == CameraType.Reflection || camera.cameraType == CameraType.Preview) return;
         if (!reflectionTarget) return;
 
-        UpdateReflectionCamera (camera);
-        CreateReflectionTexture (camera);
+        UpdateReflectionCamera(camera);
+        CreateReflectionTexture(camera);
 
-        var data = new PlanarReflectionSettingData ();
-        data.Set ();
-        BeginPlanarReflections?.Invoke (context, _reflectionCamera);
-        if (_reflectionCamera.WorldToViewportPoint (reflectionTarget.transform.position).z < 100000) UniversalRenderPipeline.RenderSingleCamera (context, _reflectionCamera);
-        data.Restore ();
-        Shader.SetGlobalTexture (_planarReflectionTextureId, _reflectionTexture);
+        var data = new PlanarReflectionSettingData();
+        data.Set();
+        BeginPlanarReflections?.Invoke(context, _reflectionCamera);
+        var pos = _reflectionCamera.WorldToViewportPoint(reflectionTarget.transform.position);
+
+        if (pos.x > 0 && pos.x < UnityEngine.Screen.width && pos.y >0 && pos.y < UnityEngine.Screen.height && !_reflectionCamera.orthographic) 
+        {
+            //屏幕大小
+            UniversalRenderPipeline.RenderSingleCamera(context, _reflectionCamera);
+        }
+        data.Restore();
+        Shader.SetGlobalTexture(_planarReflectionTextureId, _reflectionTexture);
     }
 
-    class PlanarReflectionSettingData {
+    class PlanarReflectionSettingData
+    {
         private readonly bool fog;
         private readonly int maximumLODLevel;
         private readonly float lodBias;
 
-        public PlanarReflectionSettingData () {
+        public PlanarReflectionSettingData()
+        {
             fog = RenderSettings.fog;
             maximumLODLevel = QualitySettings.maximumLODLevel;
             lodBias = QualitySettings.lodBias;
         }
 
-        public void Set () {
+        public void Set()
+        {
             GL.invertCulling = true;
             RenderSettings.fog = false;
             QualitySettings.maximumLODLevel = 1;
             QualitySettings.lodBias = lodBias * 0.5f;
         }
 
-        public void Restore () {
+        public void Restore()
+        {
             GL.invertCulling = false;
             RenderSettings.fog = fog;
             QualitySettings.maximumLODLevel = maximumLODLevel;
@@ -205,7 +237,8 @@ public class PlanarReflectionManager : MonoBehaviour {
         }
     }
 
-    public static void CalculateReflectionMatrix (ref Matrix4x4 reflectionMatrix, Vector4 plane) {
+    public static void CalculateReflectionMatrix(ref Matrix4x4 reflectionMatrix, Vector4 plane)
+    {
         reflectionMatrix.m00 = (1F - 2F * plane[0] * plane[0]);
         reflectionMatrix.m01 = (-2F * plane[0] * plane[1]);
         reflectionMatrix.m02 = (-2F * plane[0] * plane[2]);
@@ -227,13 +260,16 @@ public class PlanarReflectionManager : MonoBehaviour {
         reflectionMatrix.m33 = 1F;
     }
 
-    public static Vector3 ReflectPosition (Vector3 pos) {
-        var newPos = new Vector3 (pos.x, -pos.y, pos.z);
+    public static Vector3 ReflectPosition(Vector3 pos)
+    {
+        var newPos = new Vector3(pos.x, -pos.y, pos.z);
         return newPos;
     }
 
-    public static bool Compare (Vector2 a, Vector2 b) {
+    public static bool Compare(Vector2 a, Vector2 b)
+    {
         return a.x == b.x && a.y == b.y;
     }
+
 
 }
