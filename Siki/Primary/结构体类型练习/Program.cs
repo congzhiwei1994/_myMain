e68955b47aab74029e100a68a749f477f7ce2954
Congzhiwei1994 <z1039329737@qq.com>
2021-12-16 23:47:11 +0800

    结构体

---
+++ Siki/Primary/结构体类型练习/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace 结构体类型练习
+{
+    // 定义一个表示路径的结构，路径有一个方向和距离组成，假定方向只能是东西南北
+    enum Dir
+    {
+        West,
+        East,
+        North,
+        South
+    }
+    struct Path
+    {
+        public Dir dir;
+        public float distance;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Path path;
+            path.dir = Dir.North;
+            path.distance = 1000;
+        }
+    }
+}
